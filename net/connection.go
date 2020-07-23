493001a42a51be03d8968c297ad3a26be81b7d85
xiaoyu-0814 <pasiyu0814@163.com>
2020-07-23 17:30:53 +0800

    链接模块抽象层实现

--- net/connection.go
+++ net/connection.go
@@ -1,6 +1,7 @@
 package net
 
 import (
+	"fmt"
 	"net"
 	"zinx/iface"
 )
@@ -35,4 +36,67 @@ func NewConnection(conn *net.TCPConn,ConnId uint32, callbackApi iface.HandleFunc
 		handleAPI: callbackApi,
 		ExitChan:  make(chan bool,1),
 	}
+	return c
+}
+
+var _ iface.IConnection = new(Connection)
+
+func (c *Connection) StartReader()  {
+	fmt.Println("reader go is running ")
+	defer c.Stop()
+	defer fmt.Printf("connID=%d,Reader is exit,remote addr is %s\n",c.ConnId,c.GetRemoteAddr().String())
+
+	for {
+		//读取客户端的数据，最大512
+		buf := make([]byte,512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("read buf err:",err)
+			continue
+		}
+
+		//调用当前链接所绑定的HandleAPI
+		if err := c.handleAPI(c.Conn,buf,cnt);err != nil {
+			fmt.Printf("ConnID=%d,handle error:%s\n",c.ConnId,err.Error())
+			break
+		}
+	}
+}
+func (c *Connection) Start() {
+	fmt.Println("conn start()... connId = ",c.ConnId)
+
+	//启动从当前链接的读数据业务
+	go c.StartReader()
+}
+
+func (c *Connection) Stop() {
+	fmt.Println("Conn Stop()... ConnID = ",c.ConnId)
+
+	//如果当前链接已经关闭
+	if c.isClosed == true {
+		return
+	}
+	c.isClosed = true
+
+	//关闭链接
+	c.Conn.Close()
+	//关闭管道，回收资源
+	close(c.ExitChan)
+}
+
+//获取当前链接的绑定socket conn
+func (c *Connection) GetTCPConnection() *net.TCPConn {
+	return c.Conn
+}
+
+func (c *Connection) GetConnID() uint32 {
+	return c.ConnId
+}
+
+func (c *Connection) GetRemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
+}
+
+func (c *Connection) Send(data []byte) error {
+	panic("implement me")
 }
